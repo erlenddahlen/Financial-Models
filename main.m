34a2149926741817e16541ac36ecc3e4a3cd6fab
Erlend Dahlen <37074633+erlenddahlen@users.noreply.github.com>
2020-09-24 17:49:52 +0200

    Small changes
    Bug in the formula paying back to capitalists. Had to remove "r" from the numerator

--- main.m
+++ main.m
@@ -1,23 +1,25 @@
-i = 4; %rentesats
-r = 1; %nedbetalingsrate
-sigma = 0.5; %sparekoeffisient
-Tw = 5; %tidskonstant workers
-Tk = 20; %tidskonstant capitalists
-Td = 10; %tidskonstant 
+i = 0.04; %rentesats
+r = 0.1; %nedbetalingsrate
+sigma = 0.3; %sparekoeffisient
+Tw = 0.01; %tidskonstant workers
+Tk = 2; %tidskonstant capitalists
+Td = 0; %tidskonstant 
 
-sim_time = 250;
+sim_time = 200;
 
-out = sim('richvspoor', 200:sim_time);
+out = sim('richvspoor', sim_time);
 
 figure('rend','painters','pos',[10 10 750 400])
 hold on;
 plot(out.Ck, "b");
 plot(out.Cw, "r");
-plot(out.D, "g");
+%plot(out.Loans, "g");
+%plot(out.D, "g");
 title("Rich vs poor");
 xlabel("time [s]");
 ylabel("Debt");
 grid on;
 hold off;
-legend({"Ck","Cw","D"}, "Location", "northeast");
-%legend({"D"}, "Location", "northeast");
+%legend({"Ck","Cw","D"}, "Location", "northeast");
+legend({"Ck", "Cw"}, "Location", "northeast");
+%legend({"Loans"}, "Location", "northeast");
